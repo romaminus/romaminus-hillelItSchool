52c87cbd3dc31977dd9190c42e87624cd6186b91
romaminus <romaminus19@gmail.com>
2025-03-14 13:44:33 +0200

    added hw 3.2

--- hw/hw2/script.js
+++ hw/hw2/script.js
@@ -13,3 +13,11 @@ console.log(
     typeof {}
 );
 
+// Отримати від користувача 3 рядки та вивести їх у 
+// довільному порядку однією командою (шаблонні рядки);
+
+let firstStr = prompt('ent your name', 'Arthur'),
+    secondStr = prompt('ent your second name', 'Conan'),
+    thirdStr = prompt('ent your surname', 'Doyle');
+
+console.log(`Sir ${firstStr} ${secondStr} ${thirdStr}`);

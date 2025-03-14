8b5f70db6bde57d1d87087fbd73d49438e9b9ba5
romaminus <romaminus19@gmail.com>
2025-03-14 14:55:12 +0200

    added hw 3.3

--- hw/hw2/script.js
+++ hw/hw2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//              3.1
 //Додати туди невеликий скрипт, мета якого – 
 // вивести всі відомі вам типи даних у консоль. 
 // Використовуючи оператор typeof та console.log
@@ -10,9 +10,11 @@ console.log(
     typeof true,
     typeof undefined,
     typeof null + "(null)",
-    typeof {}
+    typeof {},
+    typeof 100n,
+    typeof Symbol()
 );
-
+//              3.2
 // Отримати від користувача 3 рядки та вивести їх у 
 // довільному порядку однією командою (шаблонні рядки);
 
@@ -21,3 +23,20 @@ let firstStr = prompt('ent your name', 'Arthur'),
     thirdStr = prompt('ent your surname', 'Doyle');
 
 console.log(`Sir ${firstStr} ${secondStr} ${thirdStr}`);
+
+//               3.3
+//Розкласти за цифрами п'ятизначне число і 
+// вивести у вихідному порядку через пробіл. Приклад:
+//10369
+//1 0 3 6 9
+let num = 10369;
+
+function modifierNum(num) {
+    let toStr = String(num);
+    let str = '';
+    for (let i = 0; i < toStr.length; i++) {
+        str += toStr[i] + ' ';
+    }
+    return str;
+}
+console.log(modifierNum(num));

406dadb52ab0e72817fb444e66a6d3cc598771bd
romaminus <romaminus19@gmail.com>
2025-03-14 13:29:38 +0200

    added hw2

---
+++ hw/hw2/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Додати туди невеликий скрипт, мета якого – 
+// вивести всі відомі вам типи даних у консоль. 
+// Використовуючи оператор typeof та console.log
+
+console.log(
+    typeof 'str',
+    typeof 57,
+    typeof true,
+    typeof undefined,
+    typeof null + "(null)",
+    typeof {}
+);
+

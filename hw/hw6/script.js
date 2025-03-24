ce424dacd47a11e022f2ce33f8ece99349ff984a
romaminus <romaminus19@gmail.com>
2025-03-24 15:14:07 +0200

    added hw 6.1

--- hw/hw6/script.js
+++ hw/hw6/script.js
@@ -5,16 +5,31 @@
 //Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом.
 // 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
 
-function deleteLetter(words, deleteList) {
-    let str = '';
-    for (let i = 0; i < deleteList.length; i++) {
-        if (!str) {
-            str = words.replaceAll(deleteList[i], '');
-        }else{
-            str = str.replaceAll(deleteList[i], '');
-        };
-    }
-    return str;
-}
-console.log(deleteLetter('hello world', ['h', 'o']));
+// function deleteLetter(words, deleteList) {
+//     let str = '';
+//     for (let i = 0; i < deleteList.length; i++) {
+//         if (!str) {
+//             str = words.replaceAll(deleteList[i], '');
+//         }else{
+//             str = str.replaceAll(deleteList[i], '');
+//         };
+//     }
+//     return str;
+// }
+// console.log(deleteLetter('hello world', ['h', 'o']));
 
+//                        6.1
+
+//Дано масив з елементами різних типів. Створити функцію яка вираховує середнє 
+// арифметичне лише числових елементів даного масиву.
+
+function middleNumber(arr) {
+    let num = arr.filter(el=>typeof el === 'number');
+    let middle = 0;
+    num.forEach(element => {
+        middle += element;
+    });
+    return middle / num.length;
+}
+
+console.log(middleNumber([1, 5, 4, 'gjk', true, null, 6,]));

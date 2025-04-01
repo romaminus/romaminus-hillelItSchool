f15ccde7438ab3b07b0b884cd46fa84a644951b0
romaminus <romaminus19@gmail.com>
2025-04-01 15:43:57 +0300

    added hw 7.2

--- hw/hw7/script.js
+++ hw/hw7/script.js
@@ -8,19 +8,40 @@
 // console.log(sum(10)); // 20
 // console.log(sum(7)); // 27
 
-function sumF() {
-    let newNum = 0;
+// function sumF() {
+//     let newNum = 0;
     
-    function adding(num) {
-        return newNum += num;
+//     function adding(num) {
+//         return newNum += num;
+//     };
+
+//     return adding;
+// }
+// let sum = sumF();
+
+// console.log(sum(4)); // 4
+// console.log(sum(6)); // 10
+// console.log(sum(10)); // 20
+// console.log(sum(7)); // 27
+
+//                  7.2
+// Створити функцію для розрахунку добутку двох чисел, що викликається так: 
+// name(5)(2). Функція повинна повертати результат (у середині функції не має бути консоль лога!)
+
+function curry(f) {
+    return function(a) {
+      return function(b) {
+        return f(a, b);
+      };
     };
+  }
 
-    return adding;
-}
-let sum = sumF();
-let sum2 = sumF();
+function summ(a, b) {
+    return a * b;
+  }
+let curriedSum = curry(summ);
 
-console.log(sum(4)); // 4
-console.log(sum(6)); // 10
-console.log(sum(10)); // 20
-console.log(sum(7)); // 27
+alert( curriedSum(5)(2) );
+
+
+//                  7.3

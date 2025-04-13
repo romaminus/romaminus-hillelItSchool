ce607cd659dd8b191591df1d04af7766bd32f751
romaminus <romaminus19@gmail.com>
2025-04-13 09:46:13 +0300

    added hw 11.2/3

--- hw/hw11/script.js
+++ hw/hw11/script.js
@@ -22,4 +22,33 @@ for (let i = 1; i <= size; i++) {
         td.innerText = `${i*j}`
         tr.append(td)
     }
-}
+};
+
+//        hw 11.2
+
+//Є блок із текстом на сторінці та кнопка. При натисканні на кнопку текст змінює колір. 
+//При повторному натисканні – повертається попередній колір
+
+let text = document.querySelector('.text'),
+    changeColorBtn = document.querySelector('.change__btn');
+
+changeColorBtn.addEventListener('click', function () {
+    text.classList.toggle('changed');
+});
+
+//        hw 11.3
+
+//Покласти в папку будь-які зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5.jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg. 
+//Вивести зображення, отримане випадковим чином (Math.random)
+
+let changeNumberBtn = document.querySelector('.change__number__btn'),
+    images = document.querySelectorAll('.img'),
+    lastIndex = 0;
+
+changeNumberBtn.addEventListener('click', function(){
+    let randomNum = Math.floor(Math.random() * 9);
+    images[lastIndex].classList.remove('active');
+    images[randomNum].classList.add('active');
+    lastIndex = randomNum;
+})
+
